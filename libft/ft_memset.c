e8f2611dd5a087c692b0aa1d8438e7d893d547d5
Tandra Melina <tmelina@un-m2.kzn.21-school.ru>
2020-10-31 18:13:27 +0300

    final campus push 31.10

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: tmelina <tmelina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 16:11:07 by tmelina           #+#    #+#             */
-/*   Updated: 2020/10/31 17:02:51 by tmelina          ###   ########.fr       */
+/*   Updated: 2020/10/31 18:12:23 by tmelina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,28 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned char* sequence;
-	unsigned char ch;
+	unsigned char *sequence;
+	unsigned int ch;
+	unsigned int i;
 
 	ch = c;
 	sequence = s;
-	while (n > 0)
+	i = 0;
+	while (i < n)
 	{
-		sequence = &ch;
-		sequence++;
-		n--;
+		*sequence++ = ch;
+		i++;
 	}
 	return (s);
 }
 
-// int		main()
-// {
-// 	// char str[] = "almost every programmer should know memset!";
-// 	char str[] = "almo";
-// 	memset (str,'-',6);
-// 	puts (str);
-// 	ft_memset (str,'-',6);
-// 	puts (str);
-// 	return 0;
-// }
+int		main()
+{
+	// char str[] = "almost every programmer should know memset!";
+	char str[] = "noanimehere";
+	// memset (str,'-',25);
+	// puts (str);
+	ft_memset (str,'A', 55);
+	puts (str);
+	return 0;
+}
