336d7c7d3200e8474f185b122d4c49d6164c8da4
Ilya Pavlov <ilya18052000@gmail.com>
2020-11-09 18:01:40 +0300

    last commit from work

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,22 @@
 /*   By: tmelina <tmelina@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 13:38:52 by tmelina           #+#    #+#             */
-/*   Updated: 2020/11/09 13:41:14 by tmelina          ###   ########.fr       */
+/*   Updated: 2020/11/09 18:01:15 by tmelina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void*	ft_memspy(void* dest, const void* src, size_t size)
+void*	ft_memcpy(void* dest, const void* src, size_t size)
 {
-	
+	unsigned char *s;
+	unsigned char *d;
+	size_t i;
+
+	s = src;
+	d = dest;
+	i = -1;
+	while (++i < size)
+		d[i] = s[i];
+	return (d);
 }
