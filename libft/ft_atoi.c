8a7b7e19cfa9607a80a8b9a84190316b746b5619
Ilya Pavlov <ilya18052000@gmail.com>
2020-11-09 15:34:39 +0300

    atoi need to be polished

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -3,49 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seogroup <seogroup@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tmelina <tmelina@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/09 12:11:02 by seogroup          #+#    #+#             */
-/*   Updated: 2020/11/09 12:52:05 by seogroup         ###   ########.fr       */
+/*   Created: 2020/11/09 13:38:33 by tmelina           #+#    #+#             */
+/*   Updated: 2020/11/09 15:34:06 by tmelina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		isnegative(int neg)
-{
-	if (neg == 1)
-		return (-1);
-	if (neg == 0)
-		return (1);
-	return (0);
-}
-
 int	ft_atoi(const char *str)
 {
-    int res;
+    unsigned long int res;
     int i;
     int neg;
 
     res = 0;
     i = -1;
-    neg = 0;
+    neg = 1;
     while (str[++i] != '\0')
     {
-        if(str[i] == '-')
-            neg++;
-        if(str[i] >= 48 && str[i] <= 57)
+        neg = str[i] == '-' ? -1 : 1;
+        if(str[++i] >= 48 && str[i] <= 57)
         {
-            res = res * 10 + (str[i] - '0');
-            if (str[i + 1] < 48 || str[i + 1] > 57)
-                return (res * isnegative(neg));
+            while (str[i] >= 48 && str[i] <= 57)
+            {
+                res = res * 10 + (str[i] - '0');
+                if (str[i] < 48 || str[i] > 57)
+                    return (res * neg);
+                i++;
+            }
+            if (res > 2147483647 || res < -2147483647)
+                return (0);
+            return (res * neg);
         }
+        if (res > 2147483647 || res < -2147483647)
+            return (-1);
+        return (0);
     }
-    return (res * isnegative(neg));
+    return (res * neg);
 }
 
 int main(){
-	char *smth = "023fds21";
-	printf("%d\n", atoi(smth));
-	printf("%d\n", ft_atoi(smth));
+    char *smth = "-999999999999999";
+    printf("%d\n", atoi(smth));
+    printf("%d\n", ft_atoi(smth));
 }
