70c171bae32d331fd0d1924fa3d8da60b872cd1b
Ilya Pavlov <ilya18052000@gmail.com>
2020-11-09 12:05:54 +0300

    working cat again

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,53 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmelina <tmelina@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: tmelina <tmelina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 12:02:47 by tmelina           #+#    #+#             */
-/*   Updated: 2020/11/08 19:56:51 by tmelina          ###   ########.fr       */
+/*   Updated: 2020/11/02 12:28:37 by tmelina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
+#include <bsd/string.h>
 #include <unistd.h>
 #include "libft.h"
 
+size_t ft_get_remsize (char *arr, size_t buffsize)
+{
+	char *s;
+	size_t remsize;
+
+	s = (char *)arr;
+	remsize = buffsize;
+	while (*s++ != '\0')
+	{
+		remsize--;
+	}
+	return (remsize);
+
+}
+
 size_t	ft_strlcat(char *dest, const char *src,  size_t size)
 {
-	char *ldest;
-	char *lsrc;
-	size_t ix;
-	size_t iy;
+	char	*s;
+	size_t pointer; // сколько места есть в буфере
+	int i;
 
-	ldest = dest;
-	lsrc = src;
-	ix = size;
-	while (ldest != '\0' && ix-- > 0)
-		ldest++;
-	iy = ldest - dest;
-	ix -= iy;
-	if (ix == 0)
-		return (iy + ft_strlen(src));
-	while (*lsrc != '\0')
-		if (ix-- != 1)
-			*ldest++ = *lsrc;
-		src++;
-	*dest = '\0';
-	return(ldest + (lsrc - src));
+	i = 0;
+	s = (char *)src;
+	puts(dest);
+	pointer = ft_get_remsize(dest, size);
+	pointer = size - pointer;
+	while (src[i] != '\0')
+		dest[pointer++] = s[i++];
+	return (pointer);
 }
 
 int	main()
 {
-	char *buf= "the cake is a lie !\0I'm hidden lol\r\n";
-	char p1[40]= "there is no stars in the sky";
-	char p2[40]= "there is no stars in the sky";
-	size_t s1, s2;
+	char src[] = "ksss1";
+	char dest[21] = "adsf";
+	// char dest1[21] = "adsf";
+	int x = ft_strlcat(dest, src, 21);
+	// int x1 = strlcat(dest1, src, 21);
 
+	printf("%d:%s\n", x, dest);
+	// printf("%d:%s\n", x1, dest1);
 
-	s1 = strlcat(p1, buf, 0);
-	printf("%zu:%s\n", s1, p1);
-	s2 = ft_strlcat(p2, buf, 0);
-	printf("%zu:%s\n", s2, p2);
 	return 0;
 }
