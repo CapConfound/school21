2f09d66288d9ede7f5faf77d40e641117f9291cd
Ilya Pavlov <ilya@Ilyas-MacBook-Pro.local>
2020-11-10 13:46:17 +0300

    changes to atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,46 +6,54 @@
 /*   By: tmelina <tmelina@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 13:38:33 by tmelina           #+#    #+#             */
-/*   Updated: 2020/11/10 11:18:18 by tmelina          ###   ########.fr       */
+/*   Updated: 2020/11/10 13:45:56 by tmelina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int	return_atoi(int res, int neg)
+{
+	if (res < -2147483648 || res > 2147483647)
+		return (-1);
+	return (res * neg);
+}
 
 int	ft_atoi(const char *str)
 {
-    long int res;
-    int i;
-    int neg;
+	int res;
+	int i;
+	int neg;
+	int j;
 
-    res = 0;
-    i = -1;
-    neg = 1;
-    while (str[++i] != '\0')
-    {
-        neg = str[i] == '-' ? -1 : 1;
-        if(str[++i] >= 48 && str[i] <= 57)
-        {
-            while (str[i] >= 48 && str[i] <= 57)
-            {
-                res = res * 10 + (str[i] - '0');
-                if (str[i] < 48 || str[i] > 57)
-                    return (res * neg);
-                i++;
-            }
-            if (res > 2147483647 || res < -2147483647)
-                return (0);
-            return (res * neg);
-        }
-        if (res > 2147483648 || res < -2147483647)
-            return (-1);
-        return (0);
-    }
-    return (res * neg);
+	res = 0;
+	j = 0;
+	i = 0;
+	neg = 1;
+	while (str[i] != '\0' || ((str[i] >= 48 && str[i] <= 57) || (str[i] == '-')))
+		i++;
+	if (j == 0)
+	{
+		if (str[i] == '-')
+			neg = -1;
+		else
+			neg = 1;
+		j = neg == -1 ? 1 : 0;
+	}
+	while (str[++i] >= 48 && str[i] <= 57)
+	{
+		res = res * 10 + (str[i] - '0');
+		if (str[i] < 48 || str[i] > 57)
+			return (return_atoi(res, neg));
+	}
+	return (return_atoi(res, neg));
 }
 
-// int main(){
-//     char *smth = "-999999999999999";
-//     printf("%d\n", atoi(smth));
-//     printf("%d\n", ft_atoi(smth));
-// }
+int main(){
+	char *smth = "2147483647";
+	printf("%d\n", atoi(smth));
+	printf("%d\n", ft_atoi(smth));
+}
