fc6a04ae6dac2eba1c5a83abd4fa110dfb8bd139
Tandra Melina <tmelina@ge-c6.kzn.21-school.ru>
2020-11-25 21:42:32 +0300

    dddd

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmelina <tmelina@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: tmelina <tmelina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 16:34:03 by tmelina           #+#    #+#             */
-/*   Updated: 2020/11/22 22:34:30 by tmelina          ###   ########.fr       */
+/*   Updated: 2020/11/25 21:41:07 by tmelina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*subs;
 	size_t	i;
 
-	subs = malloc((len * sizeof(char)) + 1);
 	i = 0;
-	if (!subs || !s)
+	if (!s)
 		return (NULL);
 	if (start >= ft_strlen((char *)s))
-		len = 1;
+		return (NULL);
 	if (ft_strlen((char *)s) <= start)
 		i = len;
+	subs = malloc(((len + 1) * sizeof(char)));
+	if (!subs)
+		return (NULL);
 	while (i < len)
 		subs[i++] = s[start++];
-	subs[i] = '\0';
+	subs[i] = 0;
 	return (subs);
 }
+
+// int main()
+// {
+// 	char	str[] = "lorem ipsum dolor sit amet";
+// 	puts(ft_substr(str, 400, 20));
+// }
